5a5433ed9dfa580dc56ac5c2b698ff56ad2937f2
Jesus Enrique Peña Sigua <JEnrique.PS@gmail.com>
2014-04-22 20:00:25 +0200

    Update settings.py

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -19,9 +19,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'granbase',                      # Or path to database file if using sqlite3.
-        'USER': 'jenrique',                      # Not used with sqlite3.
-        'PASSWORD': 'jesus=)',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
