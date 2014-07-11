d1e052bec50e81a5834a39e6c58baf26cd654caf
Jesus Enrique Peña Sigua <JEnrique.PS@gmail.com>
2014-07-10 23:49:59 -0500

    Modificando vistas

--- hellodjango/apps/campuskit/views.py
+++ hellodjango/apps/campuskit/views.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
 #from django.template import loader, Context
 from django.shortcuts import render_to_response
 
 
 def index(request):
     return render_to_response("base.html")
-
-
-def dropbox(request):
-    return render_to_response("index.html")
